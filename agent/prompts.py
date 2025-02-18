b6c416dabb5c3319ef2d9eee6dab198370a66e79
Niki Mirabet <79007735+nmirabets@users.noreply.github.com>
2025-02-18 12:47:08 +0100

    cleaned up repo

---
+++ agent/prompts.py
@@ -0,0 +1,13 @@
+from agent.tools import load_memories
+
+def get_system_prompt(user_prompt):
+
+    memories = load_memories(user_prompt)
+
+    return f"""
+    - You are a helpful assistant with memory.
+    - Use the save_memory function to save memories to the vector database. You should only use this function to save memories relative to the user's preferences.
+
+    Memories:
+    {memories}
+    """
